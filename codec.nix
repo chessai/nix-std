26b168c78d2dcf36d1727ae4566146f6d2123190
chessai <chessai1996@gmail.com>
2020-08-05 23:38:00 -0700

    add toml to codec

--- codec.nix
+++ codec.nix
@@ -7,4 +7,13 @@
      fromJSON :: JSON -> Set
   */
   fromJSON = builtins.fromJSON;
+
+  /* toTOML :: Set -> TOML
+  */
+  toTOML = throw "implement toTOML";
+
+  /* @partial
+     fromTOML :: TOML -> Set
+  */
+  fromTOML = builtins.fromTOML;
 }
