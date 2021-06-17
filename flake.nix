0fc0d964b122369afe4a7483ea9058aea45a1341
Nicole Prindle <nprindle18@gmail.com>
2021-06-17 00:46:47 -0400

    Add test.nix to flake checks

--- flake.nix
+++ flake.nix
@@ -1,7 +1,33 @@
 {
   description = "No-nixpkgs standard library for the Nix expression language";
 
-  outputs = { self }: {
-    lib = import ./default.nix;
-  };
+  outputs = { self }:
+    let
+      attrsToList = as:
+        builtins.map (n: { name = n; value = as."${n}"; })
+          (builtins.attrNames as);
+      defaultSystems = [
+        "aarch64-linux"
+        "i686-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      eachDefaultSystem = f:
+        builtins.foldl'
+          (acc: system:
+            builtins.foldl'
+              (acc: { name, value }:
+                acc // { "${name}" = (acc.${name} or { }) // { "${system}" = value; }; }
+              )
+              acc
+              (attrsToList (f system))
+          )
+          { }
+          defaultSystems;
+    in
+    {
+      lib = import ./default.nix;
+    } // eachDefaultSystem (system: {
+      checks.nix-std-test = import ./test.nix { inherit system; };
+    });
 }
