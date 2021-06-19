91cfafba2785a844a1bb89e630b1c2f865d0031c
Nicole Prindle <nprindle18@gmail.com>
2021-06-19 02:01:03 -0400

    Use optional instead of nullable in regex
    Also fix some typos in the regex.nix docs, and simplify the
    implementation of regex.splitOn

--- test.nix
+++ test.nix
@@ -682,22 +682,22 @@ let
       match = string.unlines [
         (assertEqual
           (regex.match "([[:alpha:]]+)([[:digit:]]+)" "foo123")
-          ["foo" "123"])
+          (optional.just ["foo" "123"]))
         (assertEqual
           (regex.match "([[:alpha:]]+)([[:digit:]]+)" "foobar")
-          null)
+          optional.nothing)
       ];
 
       allMatches = assertEqual (regex.allMatches "[[:digit:]]+" "foo 123 bar 456") ["123" "456"];
 
       firstMatch = string.unlines [
-        (assertEqual (regex.firstMatch "[aeiou]" "foobar") "o")
-        (assertEqual (regex.firstMatch "[aeiou]" "xyzzyx") null)
+        (assertEqual (regex.firstMatch "[aeiou]" "foobar") (optional.just "o"))
+        (assertEqual (regex.firstMatch "[aeiou]" "xyzzyx") optional.nothing)
       ];
 
       lastMatch = string.unlines [
-        (assertEqual (regex.lastMatch "[aeiou]" "foobar") "a")
-        (assertEqual (regex.lastMatch "[aeiou]" "xyzzyx") null)
+        (assertEqual (regex.lastMatch "[aeiou]" "foobar") (optional.just "a"))
+        (assertEqual (regex.lastMatch "[aeiou]" "xyzzyx") optional.nothing)
       ];
 
       split = assertEqual (regex.split "(,)" "1,2,3") ["1" [","] "2" [","] "3"];
