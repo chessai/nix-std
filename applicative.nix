33de6379a78057864f5e0e6b0dafcc2fc96ca9d0
Nicole Prindle <nprindle18@gmail.com>
2021-06-19 04:06:21 -0400

    Add optional to functor/applicative/monad

--- applicative.nix
+++ applicative.nix
@@ -1,9 +1,11 @@
 with {
   list = import ./list.nix;
   nullable = import ./nullable.nix;
+  optional = import ./optional.nix;
 };
 
 {
   list = list.applicative;
   nullable = nullable.applicative;
+  optional = optional.applicative;
 }
--- functor.nix
+++ functor.nix
@@ -1,9 +1,11 @@
 with {
   list = import ./list.nix;
   nullable = import ./nullable.nix;
+  optional = import ./optional.nix;
 };
 
 {
   list = list.functor;
   nullable = nullable.functor;
+  optional = optional.functor;
 }
