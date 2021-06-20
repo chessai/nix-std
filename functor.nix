ef069847279ba8c3de11859f25e9b6c7ea210d6d
Nicole Prindle <nprindle18@gmail.com>
2021-06-19 20:36:23 -0400

    Add F/A/M instances for nonempty to respective files

--- functor.nix
+++ functor.nix
@@ -1,11 +1,13 @@
 with {
   list = import ./list.nix;
+  nonempty = import ./nonempty.nix;
   nullable = import ./nullable.nix;
   optional = import ./optional.nix;
 };
 
 {
   list = list.functor;
+  nonempty = nonempty.functor;
   nullable = nullable.functor;
   optional = optional.functor;
 }
