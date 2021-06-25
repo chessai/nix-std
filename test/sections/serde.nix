e1ea5f91842a6ac372c34c294dbdcf9fb4d70314
chessai <chessai1996@gmail.com>
2021-06-24 17:54:39 -0700

    add tests for toJSON/fromJSON roundtrip

--- test/sections/serde.nix
+++ test/sections/serde.nix
@@ -3,26 +3,45 @@ with std;
 
 with (import ./../framework.nix);
 
+let
+  checkRoundtrip = to: from: data: assertEqual data (from (to data));
+  checkRoundtripTOML = checkRoundtrip serde.toTOML serde.fromTOML;
+  checkRoundtripJSON = checkRoundtrip serde.toJSON serde.fromJSON;
+in
 section "std.serde" {
   toml =
-    let
-      checkRoundtrip = data:
-        assertEqual true (builtins.fromTOML (serde.toTOML data) == data);
-    in
     string.unlines [
       # basic k = v notation
-      (checkRoundtrip { foo = 1; })
+      (checkRoundtripTOML { foo = 1; })
       # inline JSON-like literals
-      (checkRoundtrip { foo = [1 2 3]; })
+      (checkRoundtripTOML { foo = [1 2 3]; })
       # basic table
-      (checkRoundtrip { foo.bar = 1; })
+      (checkRoundtripTOML { foo.bar = 1; })
       # nested tables with dots
-      (checkRoundtrip { foo.bar.baz = 1; })
+      (checkRoundtripTOML { foo.bar.baz = 1; })
       # properly escapes invalid identifiers
-      (checkRoundtrip { foo."bar.baz-quux".xyzzy = 1; })
+      (checkRoundtripTOML { foo."bar.baz-quux".xyzzy = 1; })
       # double-bracket list notation for lists of dictionaries
-      (checkRoundtrip { foo = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
+      (checkRoundtripTOML { foo = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
       # mixing dot notation and list notation
-      (checkRoundtrip { foo.bar.baz = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
+      (checkRoundtripTOML { foo.bar.baz = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
+    ];
+
+  json =
+    string.unlines [
+      # basic k = v notation
+      (checkRoundtripJSON { foo = 1; })
+      # inline JSON-like literals
+      (checkRoundtripJSON { foo = [1 2 3]; })
+      # basic table
+      (checkRoundtripJSON { foo.bar = 1; })
+      # nested tables with dots
+      (checkRoundtripJSON { foo.bar.baz = 1; })
+      # properly escapes invalid identifiers
+      (checkRoundtripJSON { foo."bar.baz-quux".xyzzy = 1; })
+      # double-bracket list notation for lists of dictionaries
+      (checkRoundtripJSON { foo = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
+      # mixing dot notation and list notation
+      (checkRoundtripJSON { foo.bar.baz = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
     ];
 }
