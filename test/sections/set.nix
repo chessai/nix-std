30ed515f34952e38d86d2c9813c9ab715be41a47
arcnmx <arcnmx@users.noreply.github.com>
2021-12-02 14:08:01 -0800

    Fill in more set tests

--- test/sections/set.nix
+++ test/sections/set.nix
@@ -3,6 +3,17 @@ with std;
 
 with (import ./../framework.nix);
 
-section "std.set" {
-  filter = assertEqual set.empty (set.filter (_: const false) { a = 0; });
+let
+  testSet = { a = 0; b = 1; c = 2; };
+in section "std.set" {
+  empty = assertEqual set.empty {};
+  keys = assertEqual ["a" "b" "c"] (set.keys testSet);
+  values = assertEqual [0 1 2] (set.values testSet);
+  map = assertEqual { a = 1; b = 2; c = 3; } (set.map (_: num.add 1) testSet);
+  filter = assertEqual { b = 1; } (set.filter (k: v: v == 1) testSet);
+  toList = assertEqual [
+    { _0 = "a"; _1 = 0; }
+    { _0 = "b"; _1 = 1; }
+    { _0 = "c"; _1 = 2; }
+  ] (set.toList testSet);
 }
