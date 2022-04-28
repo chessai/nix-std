b907dd28d13ef775af0612ab76a19edaffe0b758
arcnmx <arcnmx@users.noreply.github.com>
2022-04-28 09:34:55 -0700

    set.mapZip

--- test/sections/set.nix
+++ test/sections/set.nix
@@ -10,6 +10,7 @@ in section "std.set" {
   keys = assertEqual ["a" "b" "c"] (set.keys testSet);
   values = assertEqual [0 1 2] (set.values testSet);
   map = assertEqual { a = 1; b = 2; c = 3; } (set.map (_: num.add 1) testSet);
+  zip = assertEqual { a = [0 1]; b = [1]; c = [2]; } (set.mapZip (_: function.id) [testSet { a = 1; }]);
   filter = assertEqual { b = 1; } (set.filter (k: v: v == 1) testSet);
   toList = assertEqual [
     { _0 = "a"; _1 = 0; }
