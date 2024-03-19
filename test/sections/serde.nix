786aeb49690ea8cea0ab3b3642b329cb613e27f3
Jonathan Lorimer <jonathan_lorimer@mac.com>
2024-03-19 13:30:31 -0400

    add null example

--- test/sections/serde.nix
+++ test/sections/serde.nix
@@ -10,22 +10,26 @@ let
 in
 section "std.serde" {
   toml =
-    string.unlines [
-      # basic k = v notation
-      (checkRoundtripTOML { foo = 1; })
-      # inline JSON-like literals
-      (checkRoundtripTOML { foo = [1 2 3]; })
-      # basic table
-      (checkRoundtripTOML { foo.bar = 1; })
-      # nested tables with dots
-      (checkRoundtripTOML { foo.bar.baz = 1; })
-      # properly escapes invalid identifiers
-      (checkRoundtripTOML { foo."bar.baz-quux".xyzzy = 1; })
-      # double-bracket list notation for lists of dictionaries
-      (checkRoundtripTOML { foo = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
-      # mixing dot notation and list notation
-      (checkRoundtripTOML { foo.bar.baz = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
-    ];
+    (string.unlines 
+      [
+        # basic k = v notation
+        (checkRoundtripTOML { foo = 1; })
+        # inline JSON-like literals
+        (checkRoundtripTOML { foo = [1 2 3]; })
+        # basic table
+        (checkRoundtripTOML { foo.bar = 1; })
+        # nested tables with dots
+        (checkRoundtripTOML { foo.bar.baz = 1; })
+        # properly escapes invalid identifiers
+        (checkRoundtripTOML { foo."bar.baz-quux".xyzzy = 1; })
+        # double-bracket list notation for lists of dictionaries
+        (checkRoundtripTOML { foo = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
+        # mixing dot notation and list notation
+        (checkRoundtripTOML { foo.bar.baz = [{ bar = 1; } { bar = [{ quux = 2; }]; }]; })
+        # filters nulls
+        (assertEqual (serde.fromTOML (serde.toTOML { foo = 1; bar = null; })) { foo = 1; })
+      ]
+    );
 
   json =
     string.unlines [
